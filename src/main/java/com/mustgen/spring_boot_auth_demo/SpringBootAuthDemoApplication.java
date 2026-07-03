088742440ae86940db4fd0a68042181763cd1155
antonladeiko <ladeikoanton@gmail.com>
2026-07-03 15:21:32 +0200

    chore: initial Spring Boot project setup

---
+++ src/main/java/com/mustgen/spring_boot_auth_demo/SpringBootAuthDemoApplication.java
@@ -0,0 +1,13 @@
+package com.mustgen.spring_boot_auth_demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootAuthDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootAuthDemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mustgen/spring_boot_auth_demo/SpringBootAuthDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mustgen.spring_boot_auth_demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootAuthDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
